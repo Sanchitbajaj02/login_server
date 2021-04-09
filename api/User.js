d0ed17619707f82114cfeaa242f68f3ace434c88
Sanchit Bajaj <sanchit02@outlook.com>
2021-04-09 16:51:26 +0530

    login complete

--- api/User.js
+++ api/User.js
@@ -42,7 +42,7 @@ router.post("/signup", (req, res, next) => {
     });
   } else {
     // checking if user already exists
-    User.find({email})
+    User.find({ email })
       .then((result) => {
         if (result.length) {
           // user exists
@@ -99,6 +99,61 @@ router.post("/signup", (req, res, next) => {
 });
 
 // Signin route
-router.post("/login", (req, res, next) => {});
+router.post("/login", (req, res, next) => {
+  let { email, password } = req.body;
+
+  email = email.trim();
+  password = password.trim();
+
+  if (email === "" || password === "") {
+    res.json({
+      status: "FAILED",
+      message: "Empty credentials supplied!",
+    });
+  } else {
+    // check if user exists
+    User.find({ email })
+      .then((data) => {
+        if (data.length) {
+          // user exists
+          const hashPass = data[0].password;
+          bcrypt
+            .compare(password, hashPass)
+            .then((result) => {
+              if (result) {
+                // Password match
+                res.json({
+                  status: "SUCCESS",
+                  message: "Signin Successful",
+                  data: data,
+                });
+              } else {
+                res.json({
+                  status: "FAILED",
+                  message: "Invalid Password Entered!",
+                });
+              }
+            })
+            .catch((err) => {
+              res.json({
+                status: "FAILED",
+                message: "An error occured while comparing",
+              });
+            });
+        } else {
+          res.json({
+            status: "FAILED",
+            message: "Empty credentials entered!",
+          }); 
+        }
+      })
+      .catch((err) => {
+        res.json({
+          status: "FAILED",
+          message: "An error occured while checking for existing user",
+        });
+      });
+  }
+});
 
 module.exports = router;
